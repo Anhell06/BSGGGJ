0dce4c829eba262921b9e8919e87ff8b18050251
Konstantin_Fedoskin <konstantin.fedoskin@tarkov.com>
2026-02-01 12:10:37 +0300

    Blooking

---
+++ GGJ_2026/Assets/Scripts/Blooking.cs
@@ -0,0 +1,40 @@
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class Blooking : Screen
+{
+    public List<PhotoCardView> PhotoCardViews;
+    public TMP_Text price;
+    private void OnEnable()
+    {
+        AddPhotoCard(Game.Instance.Profile.PhotoCards.OrderBy(p => p.price * p.rating).Take(PhotoCardViews.Count).ToList());
+        price.text = "Final price: " + PhotoCardViews.Where(p => p.PhotoCard != null).Sum(p => p.PhotoCard.price).ToString();
+    }
+
+    [Button]
+    public void AddPhotoCard(List<PhotoCard> photoCard)
+    {
+        for (var i = 0; i < photoCard.Count && i < PhotoCardViews.Count; i++)
+        {
+            PhotoCardViews[i].PhotoCard = photoCard[i];
+            PhotoCardViews[i].SetImage(photoCard[i].texture2D, photoCard[i].rating, name);
+        }
+    }
+    protected override void OnShow()
+    {
+        var player = FindFirstObjectByType<Player>();
+        if (player != null)
+            player.SetGameplayInputEnabled(false);
+    }
+
+    protected override void OnHide()
+    {
+        var player = FindFirstObjectByType<Player>();
+        if (player != null)
+            player.SetGameplayInputEnabled(true);
+    }
+
+}
